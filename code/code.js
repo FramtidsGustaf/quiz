840e0cb2f6f7a53c5149aea3fc28a7969da0c43b
GustafJ <gustaf89@hotmail.se>
2020-10-10 13:25:50 +0200

    added some comment and started working on a new class

--- code/code.js
+++ code/code.js
@@ -1,3 +1,3 @@
 document.addEventListener("DOMContentLoaded", function (e) {
-  new Start(); //creates an object from the start class
+  new Start(); //creates an object from the start class as soon as the DOM is loaded
 });
